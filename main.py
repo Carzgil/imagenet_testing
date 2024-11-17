5df6cf07a8c78bed4e706759fa13c22605817cbe
carzgil <cars.gilliland@gmail.com>
2024-11-17 14:53:34 -0500

    not saving checkpoint

--- main.py
+++ main.py
@@ -443,7 +443,7 @@ def validate(val_loader, model, criterion, args):
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
+    # torch.save(state, filename)  
     if is_best:
         shutil.copyfile(filename, 'model_best.pth.tar')
 
